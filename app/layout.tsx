a1e8f634d8bd096c1eb14fb615aa05cef1e6d389
Maicol Code <paucarjhon07@outlook.com>
2024-08-29 22:23:53 -0500

    Archivos iniciales del proyecto

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import '@/app/ui/global.css'
+import { montserrat } from '@/app/ui/fonts'
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang='en'>
+      <body className={`${montserrat.className} antialiased`}>{children}</body>
     </html>
-  );
+  )
 }
